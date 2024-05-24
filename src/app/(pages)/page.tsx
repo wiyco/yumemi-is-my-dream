9508ce1f2a045cf5d13735ad7fc04da7b8f14073
wiyco <me@mail.wiyco.dev>
2024-05-24 17:29:10 +0900

    Modify: Separate chart data (fetching) and display processes

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -8,39 +8,27 @@ import { Chart } from "@/components/Chart";
 import { FullScreen } from "@/components/FullScreen";
 import { Modal, modalAtom } from "@/components/Modal";
 import { fetcher } from "@/lib/swr";
-import type {
-  ReasasPopulationCompositionPerYearResponse,
-  ReasasPrefecturesResponse,
-} from "@/types/resas";
-
-type Populations = {
-  pref: ReasasPrefecturesResponse["result"][0];
-  data: ReasasPopulationCompositionPerYearResponse["result"]["data"][0]["data"];
-};
+import type { ReasasPrefecturesResponse } from "@/types/resas";
+import {
+  getPopulationChartData,
+  type PopulationChartData,
+} from "@/utils/fetcher";
 
 export default function Page() {
   const [, setIsModalOpen] = useAtom(modalAtom);
-
-  const [newPref, setNewPref] = useState<Populations["pref"] | null>(null);
-
-  const [populations, setPopulations] = useState<Populations[]>([]);
+  const [newPref, setNewPref] = useState<PopulationChartData["pref"] | null>(
+    null
+  );
+  const [populationChartData, setPopulationChartData] = useState<
+    PopulationChartData[]
+  >([]);
 
   useEffect(() => {
     if (newPref?.prefCode) {
-      fetch(`/api/v1/population/composition/years?pref=${newPref.prefCode}`)
-        .then((res) => res.json())
-        .then((data: ReasasPopulationCompositionPerYearResponse) => {
-          setPopulations((prev) => [
-            ...prev,
-            {
-              pref: {
-                prefCode: newPref.prefCode,
-                prefName: newPref.prefName,
-              },
-              data: data.result.data[0].data, // 総人口
-            },
-          ]);
-        });
+      getPopulationChartData(newPref).then((data) => {
+        if (!data) return;
+        setPopulationChartData((prev) => [...prev, data]);
+      });
     }
   }, [newPref]);
 
@@ -65,17 +53,23 @@ export default function Page() {
   return (
     <>
       <main className="grid h-dvh w-full content-around p-6">
-        <Chart
-          xAxis={[
-            {
-              data: populations[0]?.data.map(({ year }) => year),
-            },
-          ]}
-          series={populations.map(({ pref, data }) => ({
-            data: data.map(({ value }) => value),
-            label: pref.prefName,
-          }))}
-        />
+        {populationChartData.length === 0 ? (
+          <div className="grid h-[400px] place-content-center">
+            <p className="text-center">都道府県を選択してください</p>
+          </div>
+        ) : (
+          <Chart
+            xAxis={[
+              {
+                data: populationChartData[0]?.years,
+              },
+            ]}
+            series={populationChartData.map(({ pref, populations }) => ({
+              data: populations,
+              label: pref.prefName,
+            }))}
+          />
+        )}
         <section className="grid h-16 w-full content-start justify-items-center">
           <button
             className="rounded-full bg-neutral-400 px-4 py-2.5 shadow-md dark:bg-neutral-600"
@@ -100,7 +94,7 @@ export default function Page() {
                       prefName: prefName,
                     });
                   } else {
-                    setPopulations((prev) =>
+                    setPopulationChartData((prev) =>
                       prev.filter(({ pref }) => pref.prefCode !== prefCode)
                     );
                     setNewPref(null);
