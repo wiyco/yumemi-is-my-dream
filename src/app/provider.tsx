6bcdc46d5b2ae20aafd5aa4cb75c5aa992ce06b3
wiyco <me@mail.wiyco.dev>
2024-05-25 15:11:56 +0900

    Modify: Remove unused packages related
    Use in:
    * tailwind config
    * provider

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,7 +1,5 @@
-import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 
-/** @see {@link https://nextui.org/docs/guide/installation#manual-installation} */
 /** @see {@link https://github.com/pacocoursey/next-themes} */
 
 export function Provider(props: {
@@ -10,16 +8,10 @@ export function Provider(props: {
     React.ComponentProps<typeof NextThemeProvider>,
     "children"
   >;
-  nextUIProviderProps?: Omit<
-    React.ComponentProps<typeof NextUIProvider>,
-    "children"
-  >;
 }) {
   return (
-    <NextUIProvider {...props.nextUIProviderProps}>
-      <NextThemeProvider {...props.nextThemeProviderProps}>
-        {props.children}
-      </NextThemeProvider>
-    </NextUIProvider>
+    <NextThemeProvider {...props.nextThemeProviderProps}>
+      {props.children}
+    </NextThemeProvider>
   );
 }
