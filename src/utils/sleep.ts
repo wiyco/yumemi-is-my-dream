33c14629a9f51330d37b8faa8a13974753767443
wiyco <me@mail.wiyco.dev>
2024-05-25 15:14:14 +0900

    Delete: Unused utils template related

--- src/utils/sleep.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Sleep for a given amount of time in milliseconds
- * @param ms - The amount of time to sleep in milliseconds
- */
-function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export { sleep };
