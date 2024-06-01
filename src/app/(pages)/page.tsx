4716fb173209640b3d79b36f3160043a46ca38aa
wiyco <me@mail.wiyco.dev>
2024-06-02 05:33:27 +0900

    feat: restyle chart page
    related:
    * #33 - sidebar
    * #34 - accordion

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -5,10 +5,10 @@ import "./page.scss";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
+import { Accordion } from "@/components/Accordion";
 import { Chart } from "@/components/Chart";
 import { Checkbox } from "@/components/Checkbox";
-import { FullScreen } from "@/components/FullScreen";
-import { Modal, useModal } from "@/components/Modal";
+import { Sidebar } from "@/components/Sidebar";
 import { fetcher } from "@/lib/swr";
 import type { ReasasPrefecturesResponse } from "@/types/resas";
 import {
@@ -17,7 +17,6 @@ import {
 } from "@/utils/fetcher/resas";
 
 export default function Page() {
-  const [, setIsModalOpen] = useModal();
   const [newPref, setNewPref] = useState<PopulationChartData["pref"] | null>(
     null
   );
@@ -39,23 +38,50 @@ export default function Page() {
     fetcher
   );
 
-  if (isLoading)
-    return (
-      <FullScreen>
-        <p>Loading...</p>
-      </FullScreen>
-    );
-  if (error || !data)
-    return (
-      <FullScreen>
-        <p>Error!</p>
-      </FullScreen>
-    );
-
   return (
     <>
       <main className="chart-page-main">
-        <section className="chart-population-section">
+        <Sidebar className="chart-sidebar-root">
+          <Accordion header={<h2>都道府県を選択する</h2>}>
+            <section className="grid justify-items-center p-1">
+              {isLoading || !data ? (
+                !error ? (
+                  <p>読み込み中...</p>
+                ) : (
+                  <p>エラー</p>
+                )
+              ) : (
+                <ul className="chart-prefecture-list text-sm">
+                  {data.result.map(({ prefCode, prefName }, index) => (
+                    <li key={`chart-prefecture-item-${index}`}>
+                      <Checkbox
+                        id={`chart-prefecture-${prefCode}`}
+                        onChange={(e) => {
+                          if (e.target.checked) {
+                            setNewPref({
+                              prefCode: prefCode,
+                              prefName: prefName,
+                            });
+                          } else {
+                            setPopulationChartData((prev) =>
+                              prev.filter(
+                                ({ pref }) => pref.prefCode !== prefCode
+                              )
+                            );
+                            setNewPref(null);
+                          }
+                        }}
+                      >
+                        {prefName}
+                      </Checkbox>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </section>
+          </Accordion>
+        </Sidebar>
+        <section className="chart-display-root">
           {populationChartData.length === 0 ? (
             <div className=" grid h-full w-full place-content-center">
               <p className="text-center">都道府県を選択してください</p>
@@ -75,45 +101,11 @@ export default function Page() {
                 data: populations,
                 label: pref.prefName,
               }))}
-              margin={{ left: 72, right: 16, top: 114, bottom: 48 }}
+              margin={{ left: 72, right: 16, top: 16, bottom: 128 }}
             />
           )}
         </section>
-        <section className="chart-prefecture-section">
-          <button
-            className="rounded-full bg-neutral-400 px-4 py-2.5 shadow-md dark:bg-neutral-600"
-            onClick={() => setIsModalOpen((prev) => !prev)}
-          >
-            都道府県を選択する
-          </button>
-        </section>
       </main>
-      <Modal header="都道府県の選択">
-        <ul className="chart-prefecture-list">
-          {data.result.map(({ prefCode, prefName }, index) => (
-            <li key={`chart-prefecture-item-${index}`}>
-              <Checkbox
-                id={`chart-prefecture-${prefCode}`}
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    setNewPref({
-                      prefCode: prefCode,
-                      prefName: prefName,
-                    });
-                  } else {
-                    setPopulationChartData((prev) =>
-                      prev.filter(({ pref }) => pref.prefCode !== prefCode)
-                    );
-                    setNewPref(null);
-                  }
-                }}
-              >
-                {prefName}
-              </Checkbox>
-            </li>
-          ))}
-        </ul>
-      </Modal>
     </>
   );
 }
