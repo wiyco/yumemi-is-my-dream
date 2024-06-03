f24d8f11e13b4dd3383b05d842b04fcfcff8e3de
wiyco <me@mail.wiyco.dev>
2024-06-03 14:27:55 +0900

    refactor: return all type population

--- src/utils/fetcher/resas.ts
+++ src/utils/fetcher/resas.ts
@@ -4,18 +4,13 @@ import type {
 } from "@/types/resas";
 
 type PopulationChartData = {
-  /** @see {@link https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html} */
-  type: "総人口" | "年少人口" | "生産年齢人口" | "老年人口";
   pref: ReasasPrefecturesResponse["result"][0];
-  years: ReasasPopulationCompositionPerYearResponse["result"]["data"][0]["data"][0]["year"][];
-  populations: ReasasPopulationCompositionPerYearResponse["result"]["data"][0]["data"][0]["value"][];
+  populations: ReasasPopulationCompositionPerYearResponse["result"]["data"];
 };
 
 async function getPopulationChartData({
-  type,
   pref,
 }: {
-  type: PopulationChartData["type"];
   pref: PopulationChartData["pref"];
 }): Promise<PopulationChartData | null> {
   const res = (await fetch(
@@ -37,26 +32,10 @@ async function getPopulationChartData({
     return null;
   }
 
-  const targetTypeData = res.result.data.find((d) => d.label === type);
-
-  if (!targetTypeData) {
-    console.error("No data found for the specified type", type);
-    return null;
-  }
-
   const populationChartData = {
-    type: type,
     pref: pref,
-    years: targetTypeData.data.map((d) => d.year),
-    populations: targetTypeData.data.map((d) => d.value),
+    populations: res.result.data,
   };
-
-  if (
-    populationChartData.years.length !== populationChartData.populations.length
-  ) {
-    console.error("Data length mismatch");
-    return null;
-  }
 
   return populationChartData;
 }
