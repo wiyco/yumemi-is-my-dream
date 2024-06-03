e46cf7708de87dbd2bc79ebe74b65dc747d56dcd
wiyco <me@mail.wiyco.dev>
2024-06-03 14:29:42 +0900

    feat: add population type of resas

--- src/types/resas.d.ts
+++ src/types/resas.d.ts
@@ -21,7 +21,11 @@ type ReasasPopulationCompositionPerYearResponse = ResasBaseResponse<{
   }[];
 }>;
 
+/** @see {@link https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html} */
+type ReasasPopulationType = "総人口" | "年少人口" | "生産年齢人口" | "老年人口";
+
 export type {
   ReasasPopulationCompositionPerYearResponse,
+  ReasasPopulationType,
   ReasasPrefecturesResponse,
 };
